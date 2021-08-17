2da315d1a32469a9c1c5352ba03d32fbceecb9c3
Mitchel Sarauer <mitchelsarauer@hotmail.com>
2021-08-16 20:43:28 -0600

    First commit

---
+++ src/App.svelte
@@ -0,0 +1,22 @@
+<script>
+  import Header from "./components/Header.svelte";
+  import Footer from "./components/Footer.svelte";
+</script>
+
+<Header />
+<main>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem animi
+    perferendis eligendi exercitationem laudantium harum debitis ex, eaque
+    veniam numquam ut nemo rem dolorum reiciendis recusandae quasi suscipit
+    officiis et.
+  </p>
+</main>
+<Footer />
+
+<style>
+  main {
+    max-width: 960px;
+    margin: 40px auto;
+  }
+</style>
